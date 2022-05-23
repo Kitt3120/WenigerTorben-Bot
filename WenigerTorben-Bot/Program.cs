80d82cb0689c57cfa298f85ef345eb754d66a5c3
Kitt3120 <torben@schweren.dev>
2022-05-24 01:51:51 +0200

    Printing license
    Program now prints license on startup

--- WenigerTorben-Bot/Program.cs
+++ WenigerTorben-Bot/Program.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+WenigerTorben-Bot - Der WenigerTorben-Bot für den FHDW Discord Server der Bergischen Banausen
+Copyright(C) 2022  Torben Schweren
+
+This program is free software: you can redistribute it and / or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see < https://www.gnu.org/licenses/>.
+*/
+
+using System;
 using System.IO;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -16,8 +34,9 @@ public class Program
 
     public async Task Init(string[] args)
     {
-        FileUtils.GenerateDirectories();
+        PrintLicense();
 
+        FileUtils.GenerateDirectories();
         try
         {
             config = new Config();
@@ -28,6 +47,25 @@ public class Program
             Environment.Exit(1);
         }
         await config.SaveAsync();
+    }
+
+    public static void PrintLicense()
+    {
+        Console.WriteLine($"WenigerTorben-Bot - Der WenigerTorben-Bot für den FHDW Discord Server der Bergischen Banausen{Environment.NewLine}" +
+        $"Copyright(C) 2022  Torben Schweren{Environment.NewLine}" +
+        $"{Environment.NewLine}" +
+        $"This program is free software: you can redistribute it and / or modify{Environment.NewLine}" +
+        $"it under the terms of the GNU General Public License as published by{Environment.NewLine}" +
+        $"the Free Software Foundation, either version 3 of the License, or{Environment.NewLine}" +
+        $"(at your option) any later version.{Environment.NewLine}" +
+        $"{Environment.NewLine}" +
+        $"This program is distributed in the hope that it will be useful,{Environment.NewLine}" +
+        $"but WITHOUT ANY WARRANTY; without even the implied warranty of{Environment.NewLine}" +
+        $"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the{Environment.NewLine}" +
+        $"GNU General Public License for more details.{Environment.NewLine}" +
+        $"{Environment.NewLine}" +
+        $"You should have received a copy of the GNU General Public License{Environment.NewLine}" +
+        $"along with this program.If not, see < https://www.gnu.org/licenses/>.");
     }
 }
 
