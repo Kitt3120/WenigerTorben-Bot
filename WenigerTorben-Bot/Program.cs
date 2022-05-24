95c039c578729e3563143e590accb2fdd37ceb8f
Kitt3120 <torben@schweren.dev>
2022-05-24 15:43:19 +0200

    MainAsync() static
    MainAsync() is now static.
    Main() does not need to create an instance of Program() anymore.

--- WenigerTorben-Bot/Program.cs
+++ WenigerTorben-Bot/Program.cs
@@ -28,10 +28,10 @@ public class Program
 
     public static void Main(string[] args)
     {
-        new Program().MainAsync(args).GetAwaiter().GetResult();
+        MainAsync(args).GetAwaiter().GetResult();
     }
 
-    public async Task MainAsync(string[] args)
+    public static async Task MainAsync(string[] args)
     {
         PrintLicense();
         Console.WriteLine("\n");
