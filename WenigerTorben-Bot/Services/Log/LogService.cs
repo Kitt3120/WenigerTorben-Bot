b2a87b63e948bfb1fe517e89052740bed4ca6adc
Kitt3120 <torben.schweren@gmail.com>
2022-06-01 19:29:24 +0200

    Logging improvements
    A simple throwaway console-only logger is now initialized to be used before the LogService initializes the real logger.
    Replaced some Console.WriteLine()'s with Log equivalents.
    Oh and program also doesn't start on unsupported platforms anymore.. that was a thing!

--- WenigerTorben-Bot/Services/Log/LogService.cs
+++ WenigerTorben-Bot/Services/Log/LogService.cs
@@ -22,13 +22,17 @@ public class LogService : Service, ILogService, IDisposable
         string loggingDirectory = fileService.GetAndCreateDirectory("Logs");
         string logfilePath = Path.Combine(loggingDirectory, $"{DateTime.Now:yyyy-MM-dd-mm-ss}.log");
 
+        if (Serilog.Log.Logger is not null)
+            Serilog.Log.CloseAndFlush();
+
         Serilog.Log.Logger = new LoggerConfiguration()
+                                .MinimumLevel.Debug()
                                 .WriteTo.Console()
                                 .WriteTo.File(logfilePath)
                                 .CreateLogger();
 
         Serilog.Log.Information("Logger initialized");
-        Serilog.Log.Information("Logging directory is {loggingDirectory}", loggingDirectory);
+        Serilog.Log.Debug("Logging directory is {loggingDirectory}", loggingDirectory);
     }
 
     public void Dispose() => Serilog.Log.CloseAndFlush();
