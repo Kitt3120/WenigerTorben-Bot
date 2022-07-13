cb4c71fcdf688304c607180fefbc05018ff51501
Torben Schweren <torben@schweren.dev>
2022-07-13 23:10:05 +0200

    Further improvements for AudioSession and AudioRequest
    AudioRequests now hold the ITextChannel from which the request originated. This channel is used in case the target voice channel cant be determined.
    AudioSessions now have a worker thread for the queue system. Also added some methods to manage playback.

--- WenigerTorben-Bot/Audio/AudioRequest.cs
+++ WenigerTorben-Bot/Audio/AudioRequest.cs
@@ -1,18 +1,30 @@
+using System.Threading.Tasks;
 using Discord;
 
 namespace WenigerTorbenBot.Audio
 {
     public class AudioRequest
     {
-        public IUser Requestor { get; }
-        public IAudioChannel? Channel { get; }
+        public IGuildUser Requestor { get; }
+        public IVoiceChannel? VoiceChannel { get; }
+        public ITextChannel OriginChannel { get; }
         public string Request { get; }
 
-        public AudioRequest(IUser requestor, IAudioChannel? channel, string request)
+        public AudioRequest(IGuildUser requestor, IVoiceChannel? voiceChannel, ITextChannel originChannel, string request)
         {
             Requestor = requestor;
-            Channel = channel;
+            VoiceChannel = voiceChannel;
+            OriginChannel = originChannel;
             Request = request;
+        }
+
+        public async Task<IVoiceChannel?> GetTargetChannelAsync()
+        {
+            if (VoiceChannel is not null)
+                return VoiceChannel;
+
+            await Requestor.Guild.DownloadUsersAsync(); //TODO: Check if this is actually needed
+            return Requestor.VoiceChannel;
         }
     }
 }
--- WenigerTorben-Bot/Audio/AudioSession.cs
+++ WenigerTorben-Bot/Audio/AudioSession.cs
@@ -1,21 +1,35 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Discord;
+using Discord.Audio;
+using Discord.WebSocket;
+using WenigerTorbenBot.Services;
+using WenigerTorbenBot.Services.Discord;
 
 namespace WenigerTorbenBot.Audio;
 
 public class AudioSession
 {
-    private IGuild guild;
-    private object queueLock;
-    private List<AudioRequest> queue;
+    public IGuild Guild { get; private set; }
 
-    public AudioSession(IGuild guild)
+    private readonly DiscordSocketClient discordSocketClient;
+    private readonly object queueLock;
+    private readonly List<AudioRequest> queue;
+    ManualResetEvent queueThreadPause;
+    private Thread? queueThread;
+
+    public AudioSession(IDiscordService discordService, IGuild guild)
     {
-        this.guild = guild;
-        this.queueLock = new object();
-        this.queue = new List<AudioRequest>();
+        Guild = guild;
+        this.discordSocketClient = discordService.GetWrappedClient();
+
+        queueLock = new object();
+        queueThreadPause = new ManualResetEvent(true);
+        queue = new List<AudioRequest>();
     }
 
     public void Enqueue(AudioRequest audioRequest)
@@ -38,8 +52,73 @@ public class AudioSession
                 queue.RemoveAt(id);
     }
 
-    public IReadOnlyCollection<AudioRequest> GetQueue() => queue.AsReadOnly();
+    public int GetId(AudioRequest audioRequest)
+    {
+        lock (queueLock)
+            return queue.IndexOf(audioRequest);
+    }
 
-    public IReadOnlyDictionary<int, AudioRequest> GetQueueAsDictionary() => queue.ToDictionary(audioRequest => queue.IndexOf(audioRequest)).ToImmutableDictionary();
+    public void Start()
+    {
+        if (queueThread is null || !queueThread.IsAlive)
+        {
+            queueThread = new Thread(HandleQueue);
+            queueThread.Start();
+        }
+        else Resume();
+    }
+
+    public void Pause() => queueThreadPause.Reset();
+
+    public void Resume() => queueThreadPause.Set();
+
+    public void HandleQueue()
+    {
+        while (true)
+        {
+            queueThreadPause.WaitOne();
+            IReadOnlyCollection<AudioRequest> currentQueue = GetQueue();
+            if (!currentQueue.Any())
+                break;
+
+            AudioRequest audioRequest = currentQueue.First();
+            IVoiceChannel? targetChannel = audioRequest.GetTargetChannelAsync().GetAwaiter().GetResult();
+            if (targetChannel is null)
+            {
+                audioRequest.OriginChannel.SendMessageAsync($"{audioRequest.Requestor.Mention}, I was not able to determine the voice channel you're in. Your request will be skipped.").GetAwaiter().GetResult();
+                Dequeue(audioRequest);
+                continue;
+            }
+
+            using var audioClient = targetChannel.ConnectAsync().GetAwaiter().GetResult();
+            Process ffmpeg = null; //TODO: Replace with FFmpeg Service
+            using var output = ffmpeg.StandardOutput.BaseStream;
+            using var discord = audioClient.CreatePCMStream(AudioApplication.Music);
+
+            try
+            {
+                output.CopyTo(discord);
+            }
+            finally
+            {
+                discord.Flush();
+            }
+
+        }
+
+    }
+
+    public IReadOnlyCollection<AudioRequest> GetQueue()
+    {
+        lock (queueLock)
+            return queue.AsReadOnly();
+    }
+
+    public IReadOnlyDictionary<int, AudioRequest> GetQueueAsDictionary()
+    {
+
+        lock (queueLock)
+            return queue.ToDictionary(audioRequest => queue.IndexOf(audioRequest)).ToImmutableDictionary();
+    }
 
 }
