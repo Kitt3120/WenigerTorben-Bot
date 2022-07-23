e2e457b4ef659788f2e90a2a31378ad45b07919c
Torben Schweren <torben@schweren.dev>
2022-07-24 00:34:48 +0200

    ILibraryStorage improvements
    ILibraryStorage actually is no IConfigStorage, but just an IAsyncStorage. Only the LibraryStorage implementation inherits ConfigStorage as an implementation for an IAsyncStorage with Load and Save functions already.

--- WenigerTorben-Bot/Services/Storage/Library/ILibraryStorageService.cs
+++ WenigerTorben-Bot/Services/Storage/Library/ILibraryStorageService.cs
@@ -3,5 +3,5 @@ using WenigerTorbenBot.Storage.Library;
 
 namespace WenigerTorbenBot.Services.Storage.Library;
 
-public interface ILibraryStorageService<T> : IConfigStorageService<LibraryStorageEntry<T>>
+public interface ILibraryStorageService<T> : IAsyncStorageService<LibraryStorageEntry<T>>
 { }
