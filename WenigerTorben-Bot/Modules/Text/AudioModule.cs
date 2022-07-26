9f883198bc270623c093323c87cb52230f61f98b
Torben Schweren <torben@schweren.dev>
2022-07-26 20:54:36 +0200

    AudioModule audiolist command
    Added audiolist command to list audio library of a guild.

--- WenigerTorben-Bot/Modules/Text/AudioModule.cs
+++ WenigerTorben-Bot/Modules/Text/AudioModule.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
@@ -5,6 +6,9 @@ using Serilog;
 using WenigerTorbenBot.Audio.AudioSource;
 using WenigerTorbenBot.Audio.Queueing;
 using WenigerTorbenBot.Services.Audio;
+using WenigerTorbenBot.Services.Storage.Library.Audio;
+using WenigerTorbenBot.Storage;
+using WenigerTorbenBot.Storage.Library;
 
 namespace WenigerTorbenBot.Modules.Text;
 
@@ -13,10 +17,12 @@ namespace WenigerTorbenBot.Modules.Text;
 public class AudioModule : ModuleBase<SocketCommandContext>
 {
     private readonly IAudioService audioService;
+    private readonly AudioLibraryStorageService audioLibraryStorageService;
 
-    public AudioModule(IAudioService audioService)
+    public AudioModule(IAudioService audioService, AudioLibraryStorageService audioLibraryStorageService)
     {
         this.audioService = audioService;
+        this.audioLibraryStorageService = audioLibraryStorageService;
     }
 
     [Command("play")]
@@ -30,7 +36,7 @@ public class AudioModule : ModuleBase<SocketCommandContext>
             return;
         }
 
-        IAudioSource? audioSource = AudioSource.Create(request);
+        IAudioSource? audioSource = AudioSource.Create(Context.Guild, request);
         if (audioSource is null)
         {
             await ReplyAsync("Sorry, I don't know how to handle that request.");
@@ -38,7 +44,10 @@ public class AudioModule : ModuleBase<SocketCommandContext>
         }
 
         AudioRequest audioRequest = new AudioRequest(guildUser, null, textChannel, request, audioSource);
+
+        audioRequest.AudioSource.Prepare();
         audioService.Enqueue(audioRequest);
+
         audioService.GetAudioSession(Context.Guild).Start();
 
         Log.Debug("AudioRequest {request} enqueued with AudioSourceType {audioSourceType}", request, audioSource.GetAudioSourceType());
@@ -76,6 +85,41 @@ public class AudioModule : ModuleBase<SocketCommandContext>
         Log.Debug("AudioSession for Guild {guild} resumed by user {user}", Context.Guild.Id, Context.User.Id);
         audioService.Resume(Context.Guild);
         await ReplyAsync("Audio session resumed");
+    }
+
+    [Command("audiolist")]
+    [Alias(new string[] { "al", "alist", "audiol" })]
+    [Summary("Prints a list of available audios")]
+    public async Task AudioList()
+    {
+        if (Context.User is not IGuildUser || Context.Channel is not ITextChannel)
+        {
+            await ReplyAsync("This command is only available on guilds");
+            return;
+        }
+
+        if (audioLibraryStorageService is null || audioLibraryStorageService.Status != Services.ServiceStatus.Started)
+        {
+            await ReplyAsync("The AudioLibraryStorageService was not available");
+            return;
+        }
+
+        IStorage<LibraryStorageEntry<byte[]>>? library = audioLibraryStorageService.Get(Context.Guild.Id.ToString());
+        if (library is null)
+        {
+            await ReplyAsync("No library found for this guild");
+            return;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (LibraryStorageEntry<byte[]> libraryStorageEntry in library.GetValues())
+            stringBuilder.AppendLine($"{libraryStorageEntry.Title} - {libraryStorageEntry.File}");
+
+        string reply = stringBuilder.ToString();
+        if (string.IsNullOrEmpty(reply))
+            reply = "No entries found in this guild's library.";
+
+        await ReplyAsync(reply);
     }
 
     //TODO: Skip and Remove command
