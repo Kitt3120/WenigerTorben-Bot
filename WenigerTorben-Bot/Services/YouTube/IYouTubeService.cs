ca26964c00b8e83c9ff9fb566cb20b14337576ae
Torben Schweren <torben@schweren.dev>
2022-08-26 14:02:56 +0200

    YouTubeService improvements
    Added Open-methods to access direct input streams for videos, audios and muxed versions.
    Re-ordered some methods.
    Added some comments for orientation.

--- WenigerTorben-Bot/Services/YouTube/IYouTubeService.cs
+++ WenigerTorben-Bot/Services/YouTube/IYouTubeService.cs
@@ -9,23 +9,37 @@ namespace WenigerTorbenBot.Services.YouTube;
 
 public interface IYouTubeService : IService
 {
+    //Data
     public Task<Video> GetVideoAsync(string urlOrId);
     public Task<StreamManifest> GetStreamManifestAsync(string urlOrId);
     public Task<StreamManifest> GetStreamManifestAsync(Video video);
+
+    //Download
+    public Task DownloadAsync(IStreamInfo streamInfo, string filepath);
     public Task DownloadBestVideoAsync(string urlOrId, string filepath);
     public Task DownloadBestVideoAsync(Video video, string filepath);
     public Task DownloadBestAudioAsync(string urlOrId, string filepath);
     public Task DownloadBestAudioAsync(Video video, string filepath);
     public Task DownloadBestMuxAsync(string urlOrId, string filepath);
     public Task DownloadBestMuxAsync(Video video, string filepath);
-    public Task DownloadAsync(IStreamInfo streamInfo, string filepath);
+
+    //Stream
+    public Task StreamAsync(IStreamInfo streamInfo, Stream output);
     public Task StreamBestVideoAsync(string urlOrId, Stream output);
     public Task StreamBestVideoAsync(Video video, Stream output);
     public Task StreamBestAudioAsync(string urlOrId, Stream output);
     public Task StreamBestAudioAsync(Video video, Stream output);
     public Task StreamBestMuxAsync(string urlOrId, Stream output);
     public Task StreamBestMuxAsync(Video video, Stream output);
-    public Task StreamAsync(IStreamInfo streamInfo, Stream output);
+
+    //Open
+    public Task<Stream> OpenAsync(IStreamInfo streamInfo);
+    public Task<Stream> OpenBestVideoAsync(string urlOrId);
+    public Task<Stream> OpenBestVideoAsync(Video video);
+    public Task<Stream> OpenBestAudioAsync(string urlOrId);
+    public Task<Stream> OpenBestAudioAsync(Video video);
+    public Task<Stream> OpenBestMuxAsync(string urlOrId);
+    public Task<Stream> OpenBestMuxAsync(Video video);
 
     //TODO: Wrap methods for Search, Channels, Playlists
 }
--- WenigerTorben-Bot/Services/YouTube/YouTubeService.cs
+++ WenigerTorben-Bot/Services/YouTube/YouTubeService.cs
@@ -29,63 +29,37 @@ public class YouTubeService : Service, IYouTubeService
     protected override void Initialize()
     { }
 
+    //Data
     public async Task<Video> GetVideoAsync(string urlOrId) => await youtubeClient.Videos.GetAsync(urlOrId);
-
     public async Task<StreamManifest> GetStreamManifestAsync(string urlOrId) => await youtubeClient.Videos.Streams.GetManifestAsync(urlOrId);
-
     public Task<StreamManifest> GetStreamManifestAsync(Video video) => GetStreamManifestAsync(video.Url);
 
-    public async Task DownloadBestVideoAsync(string urlOrId, string filepath)
-    {
-        IVideoStreamInfo videoStreamInfo = (await GetStreamManifestAsync(urlOrId)).GetVideoOnlyStreams().GetWithHighestVideoQuality();
-        await youtubeClient.Videos.Streams.DownloadAsync(videoStreamInfo, filepath);
-    }
-
+    //Download
+    public async Task DownloadAsync(IStreamInfo streamInfo, string filepath) => await youtubeClient.Videos.Streams.DownloadAsync(streamInfo, filepath);
+    public async Task DownloadBestVideoAsync(string urlOrId, string filepath) => await DownloadAsync((await GetStreamManifestAsync(urlOrId)).GetVideoOnlyStreams().GetWithHighestVideoQuality(), filepath);
     public Task DownloadBestVideoAsync(Video video, string filepath) => DownloadBestVideoAsync(video.Url, filepath);
-
-    public async Task DownloadBestAudioAsync(string urlOrId, string filepath)
-    {
-        IStreamInfo audioStreamInfo = (await GetStreamManifestAsync(urlOrId)).GetAudioOnlyStreams().GetWithHighestBitrate();
-        await youtubeClient.Videos.Streams.DownloadAsync(audioStreamInfo, filepath);
-    }
-
+    public async Task DownloadBestAudioAsync(string urlOrId, string filepath) => await DownloadAsync((await GetStreamManifestAsync(urlOrId)).GetAudioOnlyStreams().GetWithHighestBitrate(), filepath);
     public Task DownloadBestAudioAsync(Video video, string filepath) => DownloadBestAudioAsync(video.Url, filepath);
-
-    public async Task DownloadBestMuxAsync(string urlOrId, string filepath)
-    {
-        IStreamInfo audioStreamInfo = (await GetStreamManifestAsync(urlOrId)).GetMuxedStreams().GetWithHighestVideoQuality();
-        await youtubeClient.Videos.Streams.DownloadAsync(audioStreamInfo, filepath);
-    }
-
+    public async Task DownloadBestMuxAsync(string urlOrId, string filepath) => await DownloadAsync((await GetStreamManifestAsync(urlOrId)).GetMuxedStreams().GetWithHighestVideoQuality(), filepath);
     public Task DownloadBestMuxAsync(Video video, string filepath) => DownloadBestMuxAsync(video.Url, filepath);
 
-    public async Task DownloadAsync(IStreamInfo streamInfo, string filepath) => await youtubeClient.Videos.Streams.DownloadAsync(streamInfo, filepath);
-
-    public async Task StreamBestVideoAsync(string urlOrId, Stream output)
-    {
-        IVideoStreamInfo videoStreamInfo = (await GetStreamManifestAsync(urlOrId)).GetVideoOnlyStreams().GetWithHighestVideoQuality();
-        await youtubeClient.Videos.Streams.CopyToAsync(videoStreamInfo, output);
-    }
-
+    //Stream
+    public async Task StreamAsync(IStreamInfo streamInfo, Stream output) => await youtubeClient.Videos.Streams.CopyToAsync(streamInfo, output);
+    public async Task StreamBestVideoAsync(string urlOrId, Stream output) => await StreamAsync((await GetStreamManifestAsync(urlOrId)).GetVideoOnlyStreams().GetWithHighestVideoQuality(), output);
     public Task StreamBestVideoAsync(Video video, Stream output) => StreamBestVideoAsync(video.Url, output);
-
-    public async Task StreamBestAudioAsync(string urlOrId, Stream output)
-    {
-        IStreamInfo audioStreamInfo = (await GetStreamManifestAsync(urlOrId)).GetMuxedStreams().GetWithHighestVideoQuality();
-        await youtubeClient.Videos.Streams.CopyToAsync(audioStreamInfo, output);
-    }
-
+    public async Task StreamBestAudioAsync(string urlOrId, Stream output) => await StreamAsync((await GetStreamManifestAsync(urlOrId)).GetAudioOnlyStreams().GetWithHighestBitrate(), output);
     public Task StreamBestAudioAsync(Video video, Stream output) => StreamBestAudioAsync(video.Url, output);
-
-    public async Task StreamBestMuxAsync(string urlOrId, Stream output)
-    {
-        IStreamInfo audioStreamInfo = (await GetStreamManifestAsync(urlOrId)).GetMuxedStreams().GetWithHighestVideoQuality();
-        await youtubeClient.Videos.Streams.CopyToAsync(audioStreamInfo, output);
-    }
-
+    public async Task StreamBestMuxAsync(string urlOrId, Stream output) => await StreamAsync((await GetStreamManifestAsync(urlOrId)).GetMuxedStreams().GetWithHighestVideoQuality(), output);
     public Task StreamBestMuxAsync(Video video, Stream output) => StreamBestMuxAsync(video.Url, output);
 
-    public async Task StreamAsync(IStreamInfo streamInfo, Stream output) => await youtubeClient.Videos.Streams.CopyToAsync(streamInfo, output);
+    //Open
+    public async Task<Stream> OpenAsync(IStreamInfo streamInfo) => await youtubeClient.Videos.Streams.GetAsync(streamInfo);
+    public async Task<Stream> OpenBestVideoAsync(string urlOrId) => await OpenAsync((await GetStreamManifestAsync(urlOrId)).GetVideoOnlyStreams().GetWithHighestVideoQuality());
+    public Task<Stream> OpenBestVideoAsync(Video video) => OpenBestVideoAsync(video.Url);
+    public async Task<Stream> OpenBestAudioAsync(string urlOrId) => await OpenAsync((await GetStreamManifestAsync(urlOrId)).GetAudioOnlyStreams().GetWithHighestBitrate());
+    public Task<Stream> OpenBestAudioAsync(Video video) => OpenBestAudioAsync(video.Url);
+    public async Task<Stream> OpenBestMuxAsync(string urlOrId) => await OpenAsync((await GetStreamManifestAsync(urlOrId)).GetMuxedStreams().GetWithHighestVideoQuality());
+    public Task<Stream> OpenBestMuxAsync(Video video) => OpenBestMuxAsync(video.Url);
 
     protected override ServiceConfiguration CreateServiceConfiguration() => new ServiceConfigurationBuilder().Build();
 }
